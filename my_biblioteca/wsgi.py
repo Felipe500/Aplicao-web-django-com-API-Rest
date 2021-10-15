aa1ca1e3ea9ba25d3aa41cd77318c2f75246177e
José Felipe Ribeiro <felipe.brx.dev@gmail.com>
2021-10-15 19:21:33 -0300

    update 0.01

--- my_biblioteca/wsgi.py
+++ my_biblioteca/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for my_biblioteca project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Clingort os
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_biblioteca.settings')
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
